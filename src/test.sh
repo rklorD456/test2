894598c499da2f974889c78bef17feab50400299
rklorD456 <wermasd192@gmail.com>
2025-09-23 12:26:48 +0300

    add  test

---
+++ src/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#src/test.sh
+EXPECTED = "Hello, test!"
+OUTPUT = $(node -e "console.log(require('./src/app)('Test))")
+
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+  echo "Test passed!"
+  exit 0
+else
+  echo "Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+  exit 1
+fi
